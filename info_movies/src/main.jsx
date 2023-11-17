8d97bce054399880a6a87e08a9c1d378ab51ea17
Deivid Peres <deivid.ed.8669@gmail.com>
2023-11-17 17:48:38 -0300

    small code changes

--- info_movies/src/main.jsx
+++ info_movies/src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import App from './App.jsx'
 import './index.css'
 import Home from './pages/Home.jsx'
-import Movie from './pages/movie.jsx'
+import Movie from './pages/Movie.jsx'
 import Search from './pages/search.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
